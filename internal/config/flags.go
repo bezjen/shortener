2af32441a6f0a6b1670115390f0ea3efa4086900
bezjen <bezjen@yandex.ru>
2025-07-14 04:33:38 +0300

    fix flag description

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -6,7 +6,7 @@ var FlagRunAddr string
 var FlagShortURLArrd string
 
 func ParseFlags() {
-	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&FlagRunAddr, "a", ":8080", "port to run server")
 	flag.StringVar(&FlagShortURLArrd, "b", "http://localhost:8080", "address and port of tiny url")
 	flag.Parse()
 }
