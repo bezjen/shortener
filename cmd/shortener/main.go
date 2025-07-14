38f3b3d6c025252dcd9d1aa1e38b1186b97b9c0c
bezjen <bezjen@yandex.ru>
2025-07-14 14:47:41 +0300

    add graceful shutdown

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"github.com/bezjen/shortener/internal/config"
 	"github.com/bezjen/shortener/internal/handler"
 	"github.com/bezjen/shortener/internal/repository"
 	"github.com/bezjen/shortener/internal/service"
 	"github.com/go-chi/chi/v5"
+	"log"
 	"net/http"
 )
 
@@ -19,8 +22,15 @@ func main() {
 	r.Post("/", shortenerHandler.HandlePostShortURL)
 	r.Get("/{shortURL}", shortenerHandler.HandleGetShortURL)
 
-	err := http.ListenAndServe(config.FlagRunAddr, r)
-	if err != nil {
-		panic(err)
-	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		if err := http.ListenAndServe(config.FlagRunAddr, r); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("Server failed to start: %v", err)
+			cancel()
+		}
+	}()
+
+	<-ctx.Done()
 }
