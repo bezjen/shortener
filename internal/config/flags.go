3405c9dbcf2229047fce37f9d149b33d4e0c7f3f
bezjen <bezjen@yandex.ru>
2025-07-30 11:42:39 +0300

    parse env

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -1,16 +1,29 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 type Config struct {
-	RunAddr      string
-	ShortURLAddr string
+	RunAddr      string `env:"SERVER_ADDRESS"`
+	ShortURLAddr string `env:"BASE_URL"`
 }
 
 var FlagsConfig Config
 
 func ParseFlags() {
-	flag.StringVar(&FlagsConfig.RunAddr, "a", ":8080", "port to run server")
-	flag.StringVar(&FlagsConfig.ShortURLAddr, "b", "http://localhost:8080", "address and port of tiny url")
+	addr, addrExists := os.LookupEnv("SERVER_ADDRESS")
+	if addrExists {
+		FlagsConfig.RunAddr = addr
+	} else {
+		flag.StringVar(&FlagsConfig.RunAddr, "a", ":8080", "port to run server")
+	}
+	baseURL, baseURLExists := os.LookupEnv("BASE_URL")
+	if baseURLExists {
+		FlagsConfig.ShortURLAddr = baseURL
+	} else {
+		flag.StringVar(&FlagsConfig.ShortURLAddr, "b", "http://localhost:8080", "address and port of tiny url")
+	}
 	flag.Parse()
 }
