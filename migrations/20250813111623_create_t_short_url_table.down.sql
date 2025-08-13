d7d3264c94c55093e2a41b00b92c46dcc4f4166e
bezjen <bezjen@yandex.ru>
2025-08-13 15:17:42 +0300

    fix migrations

--- migrations/20250813111623_create_t_short_url_table.down.sql
+++ migrations/20250813111623_create_t_short_url_table.down.sql
@@ -1,3 +1,1 @@
-drop index if exists idx_short_url_short_url;
-
 drop table if exists t_short_url;
--- migrations/20250813111623_create_t_short_url_table.up.sql
+++ migrations/20250813111623_create_t_short_url_table.up.sql
@@ -1,8 +1,5 @@
 create table t_short_url(
-    id uuid not null,
     short_url varchar(8) not null,
     original_url varchar(255) not null,
     primary key (id)
 );
-
-create index idx_short_url_short_url on t_short_url(short_url);
