055f0d704d35897bf41f6044db4c1ebd064b137f
bezjen <bezjen@yandex.ru>
2025-08-13 15:21:16 +0300

    implement PostgresRepository methods

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -24,11 +24,22 @@ func NewPostgresRepository(logger *logger.Logger, databaseDSN string) (*Postgres
 }
 
 func (p *PostgresRepository) Save(ctx context.Context, shortURL string, url string) error {
+	_, err := p.db.ExecContext(ctx,
+		"insert into t_short_url(short_url, original_url) values ($1, $2)", shortURL, url)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (p *PostgresRepository) GetByShortURL(ctx context.Context, shortURL string) (string, error) {
-	return "", nil
+	row := p.db.QueryRowContext(ctx, "select original_url from t_short_url where short_url = $1", shortURL)
+	var originalUrl string
+	err := row.Scan(&originalUrl)
+	if err != nil {
+		return "", err
+	}
+	return originalUrl, nil
 }
 
 func (p *PostgresRepository) Ping(ctx context.Context) error {
