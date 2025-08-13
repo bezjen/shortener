3bf82a50ce42e43a3da74699e9be1a561f6374ed
bezjen <bezjen@yandex.ru>
2025-08-13 14:28:51 +0300

    run migrations if ds is postgres

--- internal/config/db/db.go
+++ internal/config/db/db.go
@@ -1,13 +1,19 @@
 package db
 
 import (
+	"errors"
 	"github.com/bezjen/shortener/internal/config"
 	"github.com/bezjen/shortener/internal/logger"
 	"github.com/bezjen/shortener/internal/repository"
+	"github.com/golang-migrate/migrate/v4"
 )
 
 func InitDB(logger *logger.Logger, cfg config.Config) (repository.Repository, error) {
 	if cfg.DatabaseDSN != "" {
+		if err := runMigrations(cfg.DatabaseDSN); err != nil {
+			return nil, err
+		}
+
 		repoDB, err := repository.NewPostgresRepository(logger, cfg.DatabaseDSN)
 		if err != nil {
 			return nil, err
@@ -22,4 +28,17 @@ func InitDB(logger *logger.Logger, cfg config.Config) (repository.Repository, er
 	} else {
 		return repository.NewInMemoryRepository(), nil
 	}
+}
+
+func runMigrations(databaseDSN string) error {
+	m, err := migrate.New("file://migrations", databaseDSN)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+
+	return nil
 }
---
+++ migrations/20250813111623_create_t_short_url_table.down.sql
@@ -0,0 +1,3 @@
+drop index if exists idx_short_url_short_url;
+
+drop table if exists t_short_url;
